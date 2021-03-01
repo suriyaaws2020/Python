a9c41b9693eca252a700eec124b1de39596ce3f1
suriyaaws2020 <60845269+suriyaaws2020@users.noreply.github.com>
2021-03-01 15:16:17 +0530

    Update python_101.py

--- Create/NewfolderPython/python_101.py
+++ Create/NewfolderPython/python_101.py
@@ -1,11 +1,22 @@
 #variable declaration
 X=2, Y=-3, Z=0.4
+#above declares the variables under respective variable type#
+#example X=2 is an integar, Z=0.4 is an float type#
+#print function is used to print the value#
 print(type(x))
+#the above prints the value of X as 2. Output will be 2
+#below command shows how to assign values into a variable and also determines the variable type of y
 P=type(y)
+#here the output will be an integar
 print(p)
+#== operator checks whether the values are same. comparative operator
 X==Y
 print(X==Y)
+#the output will be true or false
+#below command gets the input from user and store in the variable a and b respectively
 a=input("Enter the 1st number")
 b=input("Enter the 2nd number")
+#below command make the sum of above two values received
 c=a+b
+#below command prints the value of sum
 print(c)
